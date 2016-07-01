b6f59a3fa7e117014315702098926e3a4ab44740
sargekhan <usmankhen@gmail.com>
2016-07-01 05:13:18 +0500

    completed version

---
+++ run-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+no_of_nodes=$1
+re_check_no='^[0-9]+$'
+
+if ! [[ $no_of_nodes =~ $re_check_no ]] ; then
+   echo "error: Must pass number of docker instances to launch as the first argument" >&2; exit 1
+fi
+`sudo docker-compose scale node=${no_of_nodes}`
+sudo docker-compose up &
+
+while true; do
+    log=`sudo docker events --filter 'image=plumgrid2_node' --filter 'event=die' --filter 'event=stop' --since=5s --until=0s`
+    echo "Checking after every 3 seconds if a contianer has stopped"
+    if [ -z "$log" ]; then
+        sleep 5
+    else 
+        echo "Someone is dead, Stopping rest of the containers"
+        sudo docker stop $(sudo docker ps -q --filter ancestor=plumgrid2_node )
+        exit 1
+    fi
+done
