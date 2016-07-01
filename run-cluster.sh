7bb0d9047f5d253aed3f9aa355274c923486bfde
sargekhan <usmankhen@gmail.com>
2016-07-01 09:28:30 +0500

    -

--- run-cluster.sh
+++ run-cluster.sh
@@ -7,14 +7,14 @@ if ! [[ $no_of_nodes =~ $re_check_no ]] ; then
    echo "error: Must pass number of docker instances to launch as the first argument" >&2; exit 1
 fi
 echo "Running network create"
-sudo docker network create dockersuicidesquad2_default 
+sudo docker network create dockersuicidesquad1_default 
 echo "Running  compose"
 sudo docker-compose up -d --no-recreate
 echo "Running scale"
 `sudo docker-compose scale node=${no_of_nodes}`
 
 while true; do
-    log=`sudo docker events --filter 'image=dockersuicidesquad2_node' --filter 'event=die'  --since=5s --until=0s`
+    log=`sudo docker events --filter 'image=dockersuicidesquad1_node' --filter 'event=die'  --since=5s --until=0s`
     echo "Checking after every 3 seconds if a contianer has stopped"
     if [ -z "$log" ]; then
         sleep 5
